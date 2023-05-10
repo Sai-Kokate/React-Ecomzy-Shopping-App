196e1e4cf3b758fa205403bb63932bceb069f538
Sai-Kokate <saikokatelearning@gmail.com>
2023-05-10 13:36:54 +0530

    initial commit

---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="h-[80vh] grid place-items-center">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
